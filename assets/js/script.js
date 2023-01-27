f597c2ceeda7db50c2f12ed84be2beb3d1f3d0ab
mihey <daniil53891@yandex.com>
2023-01-27 17:09:21 +0300

    add new project anf technologies stack

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,9 @@ const technologie_shields = {
   'javascript': 'https://img.shields.io/badge/JavaScript-F7DF1E?style=flat&logo=javascript&logoColor=black',
   'postgresql': 'https://img.shields.io/badge/PostgreSQL-316192?style=flat&logo=postgresql&logoColor=white',
   'mysql': 'https://img.shields.io/badge/MySQL-00000F?style=flat&logo=mysql&logoColor=white',
+  'redis': 'https://img.shields.io/badge/Redis-DC382D?style=flat&logo=redis&logoColor=white',
   'python': 'https://img.shields.io/badge/Python-3776AB?style=flat&logo=python&logoColor=white',
+  'fastapi': 'https://img.shields.io/badge/FastAPI-109989?style=flat&logo=fastapi&logoColor=white',
   'django': 'https://img.shields.io/badge/Django-092E20?style=flat&logo=django&logoColor=white',
   'flask': 'https://img.shields.io/badge/Flask-000000?style=flat&logo=flask&logoColor=white',
   'pyqt5': 'https://img.shields.io/badge/PyQt5-41CD52?style=flat&logo=pyqt&logoColor=white',
@@ -21,8 +23,18 @@ const technologie_shields = {
   'aiogram': 'https://img.shields.io/badge/Aiogram-000000?style=flat&logo=aiogram&logoColor=white',
   'java': 'https://img.shields.io/badge/Java-ED8B00?style=flat&logo=java&logoColor=white',
   'javafx': 'https://img.shields.io/badge/JavaFX-000000?style=flat&logo=javafx&logoColor=white',
-
+  'docker': 'https://img.shields.io/badge/Docker-2496ED?style=flat&logo=docker&logoColor=white',
+  'docker-compose': 'https://img.shields.io/badge/Docker%20Compose-2496ED?style=flat&logo=docker&logoColor=white',
 }
+
+function funonload() {
+  getProjectsJson().then(data => {
+    let project_data = data;
+    addInfoOfProject(project_data);
+  });
+}
+
+// window.onload = funonload;
 
 const body = document.querySelector("body");
 
@@ -104,6 +116,7 @@ const filterItems = document.querySelectorAll("[data-filter-item]");
 const filterFunc = function (selectedValue) {
 
   for (let i = 0; i < filterItems.length; i++) {
+    console.log(selectedValue, filterItems[i].dataset.category)
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
@@ -120,7 +133,7 @@ const filterFunc = function (selectedValue) {
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
-  
+
   filterBtn[i].addEventListener("click", function () {
     let selectedValue = this.id.toLowerCase();
     selectValue.innerText = this.innerText;
@@ -183,13 +196,16 @@ for (let i = 0; i < navigationLinks.length; i++) {
 // popups
 
 const popupLinks = document.querySelectorAll(".popup-link");
+console.log(popupLinks)
 
-for(let index=0; index < popupLinks.length; index++){
+for (let index = 0; index < popupLinks.length; index++) {
   const popupLink = popupLinks[index];
-  popupLink.addEventListener("click", function(event){
+  popupLink.addEventListener("click", function (event) {
     event.preventDefault();
     const popupName = popupLink.getAttribute("href").replace("#", "");
     const curentPopup = document.getElementById(popupName);
+
+    console.log(popupLink.querySelector(".project-title").innerHTML)
 
     getProjectsJson().then(data => {
       let project_data = data[popupLink.querySelector(".project-title").innerHTML];
@@ -199,44 +215,45 @@ for(let index=0; index < popupLinks.length; index++){
 }
 
 const popupCloseIcon = document.querySelectorAll(".close-popup");
-for (let i=0; i<popupCloseIcon.length; i++){
+for (let i = 0; i < popupCloseIcon.length; i++) {
   const el = popupCloseIcon[i];
-  el.addEventListener("click", function(event){
+  el.addEventListener("click", function (event) {
     popupClose(el.closest(".popup"));
     event.preventDefault();
   });
 }
 
-function popupOpen(curentPopup, project_data){
-  if (curentPopup && unlock){
-      const popupActive = document.querySelector(".popup.open");
-      if (popupActive){
-          popupClose(popupActive, false);
-      } else {
-          bodyLock();
-      }
-      projectDescription(curentPopup, project_data);
+function popupOpen(curentPopup, project_data) {
+  console.log(project_data)
+  if (curentPopup && unlock) {
+    const popupActive = document.querySelector(".popup.open");
+    if (popupActive) {
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    projectDescription(curentPopup, project_data);
 
-      curentPopup.classList.add("open");
-      curentPopup.addEventListener("click", function(e){
-          if (!e.target.closest(".popup__content")){
-              popupClose(e.target.closest(".popup"));
-          }
-      });
+    curentPopup.classList.add("open");
+    curentPopup.addEventListener("click", function (e) {
+      if (!e.target.closest(".popup__content")) {
+        popupClose(e.target.closest(".popup"));
+      }
+    });
   }
 }
 
-function projectDescription(curentPopup, project_data){
+function projectDescription(curentPopup, project_data) {
   curentPopup.querySelector(".popup__title").getElementsByTagName(
     "h3")[0].innerHTML = project_data.name;
   curentPopup.querySelector(".popup__text").innerHTML = project_data.description;
-  curentPopup.querySelector(".popup__image").src = "../assets/images/project-" + 
-                                                    project_data.category + "-" + project_data.name + ".png";
+  curentPopup.querySelector(".popup__image").src = "../assets/images/project-" +
+    project_data.category + "-" + project_data.name + ".png";
   curentPopup.querySelector(".popup__github").href = project_data.github;
-  let service_item_icon = curentPopup.querySelector(".service-item-icon"); 
+  let service_item_icon = curentPopup.querySelector(".service-item-icon");
   let img;
   let technologie;
-  for (let i=0; i<project_data.technologies.length; i++){
+  for (let i = 0; i < project_data.technologies.length; i++) {
     img = document.createElement("img");
     technologie = project_data.technologies[i];
     img.src = technologie_shields[technologie];
@@ -245,23 +262,47 @@ function projectDescription(curentPopup, project_data){
   }
 }
 
-function popupClose(popupActive, doUnlock = true){
+function popupClose(popupActive, doUnlock = true) {
   popupActive.classList.remove("open");
   popupActive.querySelector(".service-item-icon").innerHTML = "";
   bodyUnlock();
 }
 
-function bodyLock(){
+function bodyLock() {
   body.classList.add("lock");
 }
 
-function bodyUnlock(){
+function bodyUnlock() {
   body.classList.remove("lock");
 }
 
-document.addEventListener("keydown", function(e){
-  if (e.which === 27){
-      const popupActive = document.querySelector(".popup.open");
-      popupClose(popupActive);
+document.addEventListener("keydown", function (e) {
+  if (e.which === 27) {
+    const popupActive = document.querySelector(".popup.open");
+    popupClose(popupActive);
   }
 });
+
+function addInfoOfProject(data) {
+  let project_list = document.getElementsByClassName("project-list")[0]; 
+
+  let keys = Object.keys(data);
+  for (let index = 0; index < keys.length; index++) {
+    const project = data[keys[index]];
+    project_list.innerHTML += `
+    <li class="project-item  active" data-filter-item data-category="${project.category}">
+      <a href="#popup-project" class="popup-link">
+        <figure class="project-img">
+          <div class="project-item-icon-box">
+            <ion-icon name="eye-outline"></ion-icon>
+          </div>
+          <img src="./assets/images/project-${project.category}-${project.name}.png" alt="${project.name}" loading="lazy">
+        </figure>
+        <h3 class="project-title">${project.name}</h3>
+        <p class="project-category">${project.category}</p>
+      </a>
+    </li>
+    `;
+  }
+
+}
