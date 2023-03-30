85250483da94854c6e6d251a0da0a8f55b9b8dbd
mihey <daniil53891@yandex.com>
2023-03-30 21:03:41 +0300

    add projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,26 +8,34 @@ async function getProjectsJson() {
 const unlock = true;
 
 const technologie_shields = {
+  'python': 'https://img.shields.io/badge/-Python-386e9d?style=flat&logo=Python&logoColor=ffd241',
+  'java': 'https://img.shields.io/badge/Java-ED8B00?style=flat&amp;logo=java&amp;logoColor=white',
+  'go': 'https://img.shields.io/badge/Go-00ADD8?style=flat&logo=go&logoColor=white',
+
+  'fastapi': 'https://img.shields.io/badge/FastAPI-109989?style=flat&logo=fastapi&logoColor=white',
+  'flask': 'https://img.shields.io/badge/Flask-000000?style=flat&logo=flask&logoColor=white',
+  'django': 'https://img.shields.io/badge/-Django-092E20?style=flat&logo=Django&logoColor=white',
+  'drf': 'https://img.shields.io/badge/-DRF-0C4B33?style=flat&logo=Django&logoColor=white',
+  'aiohttp': 'https://img.shields.io/badge/-aiohttp-005571?style=flat&logo=aiohttp&logoColor=white',
+
+  'mysql': 'https://img.shields.io/badge/-MySQL-4479A1?style=flat&logo=MySQL&logoColor=white',
+  'postgresql': 'https://img.shields.io/badge/-Postgresql-%232c3e50?style=flat&logo=Postgresql',
+  'redis': 'https://img.shields.io/badge/-Redis-DC382D?style=flat&logo=Redis&logoColor=white',
+  'elasticsearch': 'https://img.shields.io/badge/-Elasticsearch-005571?style=flat&logo=Elasticsearch&logoColor=white',
+  'sqlalchemy': 'https://img.shields.io/badge/-SQLAlchemy-7BAC41?style=flat&logo=SQLAlchemy&logoColor=white',
+
   'html': "https://img.shields.io/badge/HTML5-E34F26?style=flat&amp;logo=html5&amp;logoColor=white",
   'css': 'https://img.shields.io/badge/CSS3-1572B6?style=flat&amp;logo=css3&amp;logoColor=white',
   'javascript': 'https://img.shields.io/badge/JavaScript-323330?style=flat&amp;logo=javascript&amp;logoColor=F7DF1E',
-  'postgresql': 'https://img.shields.io/badge/-Postgresql-%232c3e50?style=flat&logo=Postgresql',
-  'mysql': 'https://img.shields.io/badge/-MySQL-4479A1?style=flat&logo=mysql&logoColor=ffffff',
-  'redis': 'https://img.shields.io/badge/redis-%23DD0031.svg?&style=flat&logo=redis&logoColor=white',
-  'python': 'https://img.shields.io/badge/-Python-386e9d?style=flat&logo=Python&logoColor=ffd241&',
-  'fastapi': 'https://img.shields.io/badge/FastAPI-109989?style=flat&logo=fastapi&logoColor=white',
-  'django': 'https://img.shields.io/badge/-Django-0aad48?style=flat&amp;logo=Django',
-  'django-rest-framework': 'https://img.shields.io/badge/DRF-092E20?style=flat&logo=django&logoColor=white',
-  'flask': 'https://img.shields.io/badge/Flask-000000?style=flat&logo=flask&logoColor=white',
-  'pyqt5': 'https://img.shields.io/badge/-PyQt5-09db1f?style=flat&amp;logo=PyQt5',
-  'sqlalchemy': 'https://img.shields.io/badge/-sqlalchemy-4479A7?style=flat&amp;&amp;logoColor=ffffff',
-  'aiogram': 'https://img.shields.io/badge/-Aiogram-blue?style=flat&amp;logo=',
-  'java': 'https://img.shields.io/badge/Java-ED8B00?style=flat&amp;logo=java&amp;logoColor=white',
-  'javafx': 'https://img.shields.io/badge/JavaFX-000000?style=flat&logo=javafx&logoColor=white',
-  'docker': 'https://img.shields.io/badge/-Docker-46a2f1?style=flat&amp;logo=docker&amp;logoColor=white',
-  'docker-compose': 'https://img.shields.io/badge/Docker%20Compose-2496ED?style=flat&logo=docker&logoColor=white',
-  'go': 'https://img.shields.io/badge/Go-00ADD8?style=flat&logo=go&logoColor=white',
-  'rabbitmq': 'https://img.shields.io/badge/rabbitmq-%23FF6600.svg?&style=flat&logo=rabbitmq&logoColor=white',
+
+  'rabbitmq': 'https://img.shields.io/badge/-RabbitMQ-FF6600?style=flat&logo=RabbitMQ&logoColor=white',
+  
+  'docker': 'https://img.shields.io/badge/-Docker-2496ED?style=flat&logo=Docker&logoColor=white',
+  'docker-compose': 'https://img.shields.io/badge/-Docker_Compose-2496ED?style=flat&logo=Docker&logoColor=white',
+  
+  'pyqt5': 'https://img.shields.io/badge/-PyQt5-41CD52?style=flat&logo=Qt&logoColor=white',
+  'aiogram': 'https://img.shields.io/badge/-Aiogram-0088cc?style=flat&logo=Telegram&logoColor=white',
+  'javafx': 'https://img.shields.io/badge/-JavaFX-007396?style=flat&logo=JavaFX&logoColor=white',
 }
 
 function funonload() {
