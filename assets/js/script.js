cc8969a2e9c23581f4f0824afe7b6b078de697cd
mihey <daniil53891@yandex.com>
2023-08-27 12:21:34 +0300

    add dynamically create projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -206,7 +206,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
 // popups
 
-const popupLinks = document.querySelectorAll(".popup-link");
+/* const popupLinks = document.querySelectorAll(".popup-link");
 console.log(popupLinks)
 
 for (let index = 0; index < popupLinks.length; index++) {
@@ -316,4 +316,255 @@ function addInfoOfProject(data) {
     `;
   }
 
+} */
+
+
+
+//Динамическая генерация проектов
+
+//1) создать класс карточки и модального окна
+//2) получить колличество карточек с сервера
+//3) пробежаться по циклу длинной в кол-во карточек 
+//4) в кажом цикле заполнять новую карточку, модальное окно и парсить его в верстку
+
+/* const hideCard = document.querySelectorAll('.project-item');
+
+hideCard.forEach(item => {
+  item.remove();
+}) */
+
+class Card {
+  constructor(img, category, name, descr) {
+    this.img = img;
+    this.category = category;
+    this.name = name;
+    this.descr = descr;
+  }
+
+  createNewCard() {
+    const projectList = document.querySelector('.project-list');
+
+    projectList.innerHTML += `
+    <li class="project-item  active" data-filter-item="" data-category="${this.category}">
+      <a href="#popup-project" class="popup-link">
+
+        <figure class="project-img">
+          <div class="project-item-icon-box">
+            <ion-icon name="eye-outline" role="img" class="md hydrated" aria-label="eye outline"></ion-icon>
+          </div>
+
+          <img src=${this.img} alt="Doc search" loading="lazy">
+        </figure>
+
+        <h3 class="project-title">${this.name}</h3>
+
+        <p class="project-category">${this.descr}</p>
+
+      </a>
+    </li>
+    `
+  }
+
 }
+
+
+class Modal {
+  constructor(name, descr, link, more) {
+    this.name = name;
+    this.descr = descr;
+    this.github = link;
+    this.img = more;
+  }
+
+  createNewModal() {
+    const popup = document.createElement('div')
+
+    popup.id = "popup-project";
+    popup.classList.add('popup')
+
+    popup.innerHTML = `
+    <div class="popup__main">
+      <div class="popup__body">
+        <div class="popup__content">
+          <a href="#" class="popup__close close-popup">
+            <ion-icon name="close-circle-outline"></ion-icon>
+          </a>
+          <div class="popup__title">
+            <h3>${this.name}</h3>
+          </div>
+          <!-- <img src="" alt="" class="popup__image" loading="lazy"> -->
+          <section class="service">
+
+            <h3 class="h3 service-title">Cтэк технологий</h3>
+
+            <div class="service-list">
+              <div class="service-item">
+                <div class="service-content-box">
+                  <div class="service-item-icon"></div>
+                </div>
+              </div>
+            </div>
+
+          </section>
+          <div class="popup__text">${this.descr}</div>
+          <a href=${this.github} class="popup__github social-link" target="_blank">
+            <p>GitHub&nbsp;</p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+              <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+              <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div class="popup_bottom_void"></div>
+    </div>
+    `
+
+    for (let key in this.img) {
+      const stack = document.createElement('img'),
+            icon = popup.querySelector('.service-item-icon');
+
+      stack.src = this.img[key];
+
+      icon.append(stack);
+    }
+
+    document.documentElement.append(popup);
+    return popup;
+    
+    /* this.img.forEach((img) => {
+      const stack = document.createElement('img'),
+            icon = popup.querySelector('.service-item-icon');
+
+      stack.src = img;
+      
+
+      icon.append(stack);
+      
+    }) */
+    
+
+    
+    
+  }
+}
+
+function KeepCardsWithThisDataOnly(id) {
+  const dataAtr = document.querySelectorAll('.project-item');
+
+  dataAtr.forEach(item => {
+    if (item.getAttribute('data-category') != id){
+      item.classList.remove('active');        
+    }
+  })
+
+}
+
+function takeActiveAllCards() {
+  const cards = document.querySelectorAll('.project-item');
+
+  cards.forEach(item => {
+    item.classList.add('active');
+  })
+
+}
+
+function showCategory() {
+  const category = document.querySelector('.filter-list')
+
+  category.addEventListener('click', (e) => {
+    if (e.target && e.target.getAttribute('data-filter-btn') == '') {
+      if (e.target.id == 'api') {
+        takeActiveAllCards();
+        KeepCardsWithThisDataOnly('api');
+      } else if(e.target.id == 'bot') {
+        takeActiveAllCards()
+        KeepCardsWithThisDataOnly('bot');
+      } else if(e.target.id == 'app') {
+        takeActiveAllCards();
+        KeepCardsWithThisDataOnly('app');
+      } else if(e.target.id == 'all') {
+        takeActiveAllCards();
+      }
+    }
+  })
+}
+
+const request = new XMLHttpRequest();
+
+request.open('GET','assets/projects/projects.json');
+request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+request.send();
+
+request.addEventListener('load', () => {
+    if (request.status == 200) {
+      let response = JSON.parse(request.response);
+
+      const hideCard = document.querySelectorAll('.project-item');
+
+      hideCard.forEach(item => {
+        item.remove();
+      })
+
+      let modalContainer = [],
+          i = 0;
+      for (let key in response) {
+        const newCard = new Card(response[key].img, response[key].category, response[key].name, response[key].descr);
+        newCard.createNewCard();
+        const newModal = new Modal(response[key].name, response[key].description, response[key].github, response[key].stack);
+        newModal.createNewModal();
+        /* modalContainer.push(document.querySelector('.popup'));
+        i++; */
+        /* console.log(document.querySelectorAll('#popup-project')); */
+        
+      }
+      document.querySelectorAll('.popup').forEach(item => {
+        modalContainer.push(item)
+      })
+      
+      
+
+      document.querySelector('.project-list').addEventListener('click', (e) => {
+        
+        if (!e.target.classList.contains('project-list')) {
+          const card = document.querySelectorAll('.project-item');
+          card.forEach((item, i) => {
+            if (item.contains(e.target) || e.target.classList.contains('md')) {
+              modalContainer[i].classList.add('open');
+              
+            } 
+          })
+        }
+        
+      })
+
+      document.documentElement.addEventListener('click', (e) => {
+        if (e.target && e.target.classList.contains('md') || e.target.classList.contains('popup')) {
+          modalContainer.forEach(item => {
+            item.classList.remove('open');
+          })
+        }
+      })
+
+      document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' ){
+          modalContainer.forEach(item => {
+            item.classList.remove('open');
+          })
+        }
+    })
+
+      
+
+      showCategory()
+      
+    } else {
+      document.querySelector('.project-list').innerHTML= `
+        Что-то пошло не так
+      `
+      document.querySelector('.project-list').style.color = 'white';
+    }
+})
+
+
+
